1312943c3c2d00b28cb98f14905eedff2e787c62
Arif Ibrahim <arif.ibrahm@gmail.com>
2023-09-01 16:29:09 +0600

    mega munu show and hide by hovering done

--- components/layout/MainHeader.vue
+++ components/layout/MainHeader.vue
@@ -18,7 +18,7 @@ const navbarClass = computed(() => {
 
 const navbarInnerClass = computed(() => {
     return {
-        'py-4': showNavbar.value,
+        'h-12': showNavbar.value,
     }
 })
 
@@ -47,33 +47,35 @@ onUnmounted(() => {
 })
 </script>
 <template>
-    <div class="bg-slate-50 border-b-2  main-site-padding mb-5 w-full  " :class="navbarClass">
-        <div class="flex justify-between py-10 " :class="navbarInnerClass">
-            <div class="content-color">abco</div>
-            <div class="flex gap-3">
-                <div class="relative">
-                    <div @mouseover="showElement(headerMenu.HOME)" @mouseleave="hideElement()" >
-                        <div class=" cursor-pointer border-2   border-red-500"
+    <div class="bg-indigo-300 border-b-2 main-site-padding mb-5  w-full" :class="navbarClass">
+        <div class="flex justify-between h-20 items-center" :class="navbarInnerClass">
+
+            <div class="content-color ">abco</div>
+
+            <div class="flex gap-3 relative h-full">
+              
+                    <div class="h-full" @mouseover="showElement(headerMenu.HOME)" @mouseleave="hideElement()" >
+                        <div class=" flex items-center cursor-pointer  h-full"
                         >{{ headerMenu.HOME }}</div>
-                    <CardMenuItem1 v-if="showMenu === headerMenu.HOME">
-                    </CardMenuItem1>
+                        <CardMenuItem1 v-if="showMenu === headerMenu.HOME">
+                        </CardMenuItem1>
                     </div>
-                </div>
-                <div class="relative">
-                   <div @mouseover="showElement(headerMenu.ABOUT)" @mouseleave="hideElement()">
-                    <div class=" cursor-pointer border-2   border-red-500"
+
+                    <div class="h-full" @mouseover="showElement(headerMenu.ABOUT)" @mouseleave="hideElement()" >
+                        <div class=" flex items-center cursor-pointer   border-red-500 h-full"
                         >{{ headerMenu.ABOUT }}</div>
-                    <CardMenuItem  v-if="showMenu === headerMenu.ABOUT">
-                    </CardMenuItem>
-                   </div>
-                </div>
+                        <CardMenuItem v-if="showMenu === headerMenu.ABOUT">
+                        </CardMenuItem>
+                    </div>
 
             </div>
+
             <div class="flex">
                 <div>icon1</div>
                 <div>icon2</div>
                 <div>icon3</div>
             </div>
+
         </div>
     </div>
 </template>
