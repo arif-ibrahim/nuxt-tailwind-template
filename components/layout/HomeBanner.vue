d6cb06a8c837cbd26851ff03a1f3872baab79281
Ibrahim <ibrahim.jatri@gmail.com>
2023-09-11 18:12:32 +0600

    fade effect slider added

--- components/layout/HomeBanner.vue
+++ components/layout/HomeBanner.vue
@@ -1,4 +1,7 @@
 <script setup>
+const testing = (i) => {
+    console.log('adfsaaaaaaaaaaaaaaa', i);
+}
 const slides = ref(Array.from({ length: 10 }, () => {
     const r = Math.floor(Math.random() * 256)
     const g = Math.floor(Math.random() * 256)
@@ -13,26 +16,14 @@ const slides = ref(Array.from({ length: 10 }, () => {
 <template>
     <div>
 
-        <Swiper :height="500" :modules="[SwiperAutoplay, SwiperEffectCreative]" :slides-per-view="1" :loop="true"
-            :effect="'creative'" :autoplay="{
+        <Swiper :on-slide-change="testing()" :centered-slides="true" :height="500"
+            :modules="[SwiperAutoplay, SwiperEffectFade]" :slides-per-view="1" :loop="true" :effect="'fade'" :autoplay="{
                 delay: 5000,
                 disableOnInteraction: true
-            }" :creative-effect="{
-    prev: {
-        shadow: false,
-        translate: ['-20%', 0, -1]
-    },
-    next: {
-        translate: ['100%', 0, 0]
-    }
-}">
+            }">
             <SwiperSlide v-for="(slide, idx) in slides" :key="idx"
                 :style="`background-color: ${slide.bg}; color: ${slide.color}`">
-                jsdlkjflksdjlf;jsdlfjdlsjf
             </SwiperSlide>
-
-            <!-- useSwiper() within a swiper instance -->
-            <SwiperControls />
         </Swiper>
 
 
