cdab28999f3f8a3652821ae665840547c5f25417
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-12 19:32:42 +0600

    started working on main banner

--- components/layout/HomeBanner.vue
+++ components/layout/HomeBanner.vue
@@ -1,29 +1,23 @@
 <script setup>
-const testing = (i) => {
-    console.log('adfsaaaaaaaaaaaaaaa', i);
-}
-const slides = ref(Array.from({ length: 10 }, () => {
-    const r = Math.floor(Math.random() * 256)
-    const g = Math.floor(Math.random() * 256)
-    const b = Math.floor(Math.random() * 256)
-    // Figure out contrast color for font
-    const contrast = r * 0.299 + g * 0.587 + b * 0.114 > 186 ? 'black' : 'white'
 
-    return { bg: `rgb(${r}, ${g}, ${b})`, color: contrast }
-}))
+
+
 </script>
 
 <template>
     <div>
 
-        <Swiper :on-slide-change="testing()" :centered-slides="true" :height="500"
-            :modules="[SwiperAutoplay, SwiperEffectFade]" :slides-per-view="1" :loop="true" :effect="'fade'" :autoplay="{
-                delay: 5000,
-                disableOnInteraction: true
+        <Swiper :modules="[SwiperAutoplay, SwiperEffectFade]" :effect="'fade'" :crossFade="true" :slides-per-view="1"
+            :loop="true" :autoplay="{
+                delay: 30000,
+                disableOnInteraction: true,
             }">
-            <SwiperSlide v-for="(slide, idx) in slides" :key="idx"
-                :style="`background-color: ${slide.bg}; color: ${slide.color}`">
+            <SwiperSlide>
+                <div
+                    class="bg-contain bg-no-repeat h-[400px] sm:h-[200px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner2_2000x.webp')]">
+                </div>
             </SwiperSlide>
+
         </Swiper>
 
 
@@ -32,20 +26,13 @@ const slides = ref(Array.from({ length: 10 }, () => {
 
 <style>
 .swiper-slide {
-    display: flex;
-    justify-content: center;
-    align-items: center;
     font-size: 18px;
-    height: 20vh;
     font-size: 4rem;
     font-weight: bold;
     font-family: 'Roboto', sans-serif;
 }
 
-.swiper-wrapper {
-    min-width: 100vh;
-    width: 100vh;
-}
+.swiper-wrapper {}
 
 .swiper {
     z-index: -1;
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-    <LayoutHomeBanner></LayoutHomeBanner>
+    <LayoutHomeBanner v-if="loaded"></LayoutHomeBanner>
     <div class="main-site-padding">
         <div class="grid grid-cols-4 gap-4 ">
-            <div v-for="i in 50" class="bg-slate-100 h-96"></div>
+            <div v-for="i in 5" class="bg-slate-100 h-96"></div>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const loaded = ref(false)
+onMounted(() => {
+    loaded.value = true
+})
+</script>
