b26547997150fbfdc320d7797d3ce1ee59d8260f
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-15 19:32:37 +0600

    make responsive main banner

--- components/layout/HomeBanner.vue
+++ components/layout/HomeBanner.vue
@@ -8,20 +8,18 @@
     <div>
         <Swiper :modules="[SwiperAutoplay, SwiperEffectFade]" :effect="'fade'" :crossFade="true" :slides-per-view="1"
             :loop="true" :autoplay="{
-                delay: 10000,
+                delay: 3000,
                 disableOnInteraction: true,
             }">
 
             <SwiperSlide>
                 <div
-                    class="grid grid-cols-1   sm:grid-cols-4 bg-cover bg-center bg-no-repeat h-[200px] sm:h-[400px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner1_2000x.webp')]">
-                    <div></div>
-
-                    <div class="shopNow flex flex-col gap-0 sm:gap-2 justify-center items-center sm:items-start">
+                    class="grid grid-cols-1   sm:grid-cols-2 bg-cover bg-center bg-no-repeat h-[200px] sm:h-[400px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner1_2000x.webp')]">
+                    <div class="shopNow flex flex-col gap-0 sm:gap-2 justify-center items-center sm:items-end">
                         <div class="text-sm">NEW COLLECTION</div>
                         <div>
-                            <div class="text-2xl sm:text-5xl font-bold">Luxery Brands</div>
-                            <div class="text-2xl sm:text-5xl tracking-wider">Without Labels</div>
+                            <div class="text-2xl md:text-5xl font-bold">Luxery Brands</div>
+                            <div class="text-2xl md:text-5xl tracking-wider">Without Labels</div>
                         </div>
                         <div class="mt-1 sm:mt-2">
                             <button class="bg-black hover:bg-blue-600 text-white py-2 px-4 rounded cursor-pointer">
@@ -30,21 +28,17 @@
                         </div>
                     </div>
                     <div></div>
-                    <div></div>
-
-
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div
-                    class="grid grid-cols-1   sm:grid-cols-4 bg-cover bg-center bg-no-repeat h-[200px] sm:h-[400px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner2_2000x.webp')]">
-                    <div></div>
+                    class="grid grid-cols-1   sm:grid-cols-2 bg-cover bg-center bg-no-repeat h-[200px] sm:h-[400px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner2_2000x.webp')]">
                     <div></div>
                     <div class="shopNow flex flex-col gap-0 sm:gap-2 justify-center items-center sm:items-start">
                         <div class="text-sm">NEW COLLECTION</div>
                         <div>
-                            <div class="text-2xl sm:text-5xl font-bold">Luxery Brands</div>
-                            <div class="text-2xl sm:text-5xl tracking-wider">Without Labels</div>
+                            <div class="text-2xl md:text-5xl font-bold">Luxery Brands</div>
+                            <div class="text-2xl md:text-5xl tracking-wider">Without Labels</div>
                         </div>
                         <div class="mt-1 sm:mt-2">
                             <button class="bg-black hover:bg-blue-600 text-white py-2 px-4 rounded cursor-pointer">
@@ -52,13 +46,9 @@
                             </button>
                         </div>
                     </div>
-
-
-                    <div></div>
-
-
                 </div>
             </SwiperSlide>
+
 
         </Swiper>
 
