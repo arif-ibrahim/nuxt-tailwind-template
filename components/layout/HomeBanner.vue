7c584d3d49a6f4fd8638ca4e2e72940c03f10026
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-15 18:48:09 +0600

    added animation to main banner

--- components/layout/HomeBanner.vue
+++ components/layout/HomeBanner.vue
@@ -8,31 +8,53 @@
     <div>
         <Swiper :modules="[SwiperAutoplay, SwiperEffectFade]" :effect="'fade'" :crossFade="true" :slides-per-view="1"
             :loop="true" :autoplay="{
-                delay: 10000,
+                delay: 3000,
                 disableOnInteraction: true,
             }">
-            <!-- <SwiperSlide>
-                <div
-                    class="bg-cover bg-center bg-no-repeat h-[200px] sm:h-[400px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner2_2000x.webp')]">
-                </div>
-            </SwiperSlide> -->
+
             <SwiperSlide>
                 <div
-                    class="grid grid-cols-1 sm:grid-cols-4 bg-cover bg-center bg-no-repeat h-[200px] sm:h-[400px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner1_2000x.webp')]">
+                    class="grid grid-cols-1   sm:grid-cols-4 bg-cover bg-center bg-no-repeat h-[200px] sm:h-[400px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner1_2000x.webp')]">
                     <div></div>
-                    <div class="flex flex-col gap-2 justify-center items-center sm:items-start">
+
+                    <div class="shopNow flex flex-col gap-0 sm:gap-2 justify-center items-center sm:items-start">
                         <div class="text-sm">NEW COLLECTION</div>
                         <div>
-                            <div class="text-5xl font-bold">Luxery Brands</div>
-                            <div class="text-5xl tracking-wider">Without Labels</div>
+                            <div class="text-2xl sm:text-5xl font-bold">Luxery Brands</div>
+                            <div class="text-2xl sm:text-5xl tracking-wider">Without Labels</div>
                         </div>
-                        <div class="mt-2">
+                        <div class="mt-1 sm:mt-2">
                             <button class="bg-black hover:bg-blue-600 text-white py-2 px-4 rounded cursor-pointer">
                                 SHOP NOW
                             </button>
                         </div>
                     </div>
+
+
                     <div></div>
+                    <div></div>
+
+
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div
+                    class="grid grid-cols-1   sm:grid-cols-4 bg-cover bg-center bg-no-repeat h-[200px] sm:h-[400px] md:h-[492px] lg:h-[492px] xl:h-[531px] 2xl:h-[667px] w-full    bg-[url('/d1banner2_2000x.webp')]">
+                    <div></div>
+                    <div></div>
+                    <div class="shopNow flex flex-col gap-0 sm:gap-2 justify-center items-center sm:items-start">
+                        <div class="text-sm">NEW COLLECTION</div>
+                        <div>
+                            <div class="text-2xl sm:text-5xl font-bold">Luxery Brands</div>
+                            <div class="text-2xl sm:text-5xl tracking-wider">Without Labels</div>
+                        </div>
+                        <div class="mt-1 sm:mt-2">
+                            <button class="bg-black hover:bg-blue-600 text-white py-2 px-4 rounded cursor-pointer">
+                                SHOP NOW
+                            </button>
+                        </div>
+                    </div>
+
 
                     <div></div>
 
@@ -46,7 +68,25 @@
     </div>
 </template>
 
-<style>
+<style scoped>
+.shopNow {
+    transform: scale(1);
+    animation-name: shopNow;
+    animation-duration: .5s;
+}
+
+@keyframes shopNow {
+    0% {
+        transform: scale(0);
+
+    }
+
+    100% {
+        transform: scale(1);
+
+    }
+}
+
 .swiper-slide {
     font-family: 'Roboto', sans-serif;
 }
