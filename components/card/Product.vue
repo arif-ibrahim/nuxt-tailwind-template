8670384b178716a6a7e66f24d83a84d8f928e769
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-18 15:45:30 +0600

    make round icon component

--- components/card/Product.vue
+++ components/card/Product.vue
@@ -2,12 +2,8 @@
     <div
         class="flex flex-col-reverse bg-cover bg-center bg-no-repeat w-full h-[420px] bg-[url('/bag3.webp')] hover:bg-[url('/bag.webp')] hover:cursor-pointer">
         <div class="flex gap-5 justify-center mb-5">
-            <button class="rounded-full bg-white hover:bg-black hover:text-white h-9 w-9">
-                <Icon name="clarity:shopping-cart-line" />
-            </button>
-            <button class="rounded-full bg-white hover:bg-black hover:text-white h-9 w-9">
-                <Icon name="mdi:magnify" />
-            </button>
+            <UtilityIconRound icon="clarity:shopping-cart-line" />
+            <UtilityIconRound icon="mdi:magnify" />
         </div>
     </div>
 </template>
---
+++ components/utility/IconRound.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button class="rounded-full bg-white hover:bg-black hover:text-white h-9 w-9">
+            <Icon :name="iconLocal" />
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps(['icon']);
+const iconLocal = ref(props.icon)
+
+</script>
+
+<style scoped></style>
