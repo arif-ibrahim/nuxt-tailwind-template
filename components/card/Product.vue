c26af7c2a92351d13f51ec3c0e1e86226a7527cc
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-18 15:31:49 +0600

    added card product and shopping cart icon

--- components/card/Product.vue
+++ components/card/Product.vue
@@ -1,6 +1,14 @@
 <template>
-    <div class="w-full h-[420px] bg-gray-200">
-
+    <div
+        class="flex flex-col-reverse bg-cover bg-center bg-no-repeat w-full h-[420px] bg-[url('/bag3.webp')] hover:bg-[url('/bag.webp')] hover:cursor-pointer">
+        <div class="flex gap-5 justify-center mb-5">
+            <button class="rounded-full bg-white hover:bg-black hover:text-white h-9 w-9">
+                <Icon name="clarity:shopping-cart-line" />
+            </button>
+            <button class="rounded-full bg-white hover:bg-black hover:text-white h-9 w-9">
+                <Icon name="mdi:magnify" />
+            </button>
+        </div>
     </div>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-swiper'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-swiper', 'nuxt-icon',],
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
     exposeConfig: true,
