7abd375c3d5e7727e894294297c503008113d15b
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-19 17:20:11 +0600

    add cart item into side drawer

--- components/SideDrawer.vue
+++ components/SideDrawer.vue
@@ -10,7 +10,26 @@ const emit = defineEmits(['closeOverlay'])
             <div @click="emit('closeOverlay')"
                 class="absolute w-4/5 h-screen inset-y-0 left-0 bg-black opacity-60 cursor-pointer">
             </div>
-            <div class="absolute w-1/5 h-screen inset-y-0 right-0 bg-white">
+            <div class="absolute w-1/5 h-screen inset-y-0 right-0 bg-white p-3">
+                <div class="flex justify-between">
+                    <div class="text-base">Your Cart(5)</div>
+                    <div @click="emit('closeOverlay')" class="text-xl cursor-pointer">
+                        <Icon name="bx:x" />
+                    </div>
+                </div>
+                <div class="bg-gray-100 p-3 my-3 text-center">
+                    <Icon name="icon-park-outline:transporter" /><span class="text-xs ml-1">YOUR ORDER IS ELIGIBLE FOR <span
+                            class="font-medium">FREE
+                            DELIVERY.</span></span>
+
+                </div>
+                <div>
+                    <ul role="list" class="divide-y divide-gray-200">
+                        <li v-for="i in 3" class=" mt-2 pt-2 first:pt-0">
+                            <CardCartItem></CardCartItem>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
---
+++ components/card/CartItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex gap-2 min-h-fit">
+        <div class="w-1/4">
+            <img class="h-full object-cover object-center" src="/bag3.webp" alt="picture">
+        </div>
+        <div class="w-3/4">
+            <div class="text-xs">Crossedbody Bag with Chain Strap </div>
+            <div class="text-gray-500 text-xs mt-1">
+                <div class="-mb-0.5">Color: Blue</div>
+                <div>Size: M</div>
+            </div>
+            <div class="text-xs mt-1">$280</div>
+            <div class="flex gap-5 text-gray-600 text-sm mt-1">
+                <div class="border rounded-sm">
+                    <button class="border-r h-6 w-6 align-text-bottom">
+                        <Icon name="icon-park-outline:plus" />
+                    </button>
+                    <input class="w-6 h-6 text-center align-text-bottom" type="text" value="2">
+                    <button class="border-l w-6 h-6 align-text-bottom">
+                        <Icon name="icon-park-outline:minus" />
+                    </button>
+                </div>
+                <div class="cursor-pointer">
+                    <Icon name="icon-park-outline:delete" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
