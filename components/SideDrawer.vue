5a481ae2151b7299df0ec9503a9fefd02c902d10
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-29 17:12:20 +0600

    fixed side drawer height issue

--- components/SideDrawer.vue
+++ components/SideDrawer.vue
@@ -5,13 +5,13 @@ const emit = defineEmits(['closeOverlay'])
 
 
 <template>
-    <div class="inset-0 fixed z-20">
-        <div class="relative">
+    <div class="inset-0 fixed z-20 ">
+        <div class="relative h-[calc(100dvh)]">
             <div @click="emit('closeOverlay')"
-                class="absolute w-1/6 sm:w-3/6 md:w-3/5 lg:w-2/3 xl:w-9/12 2xl:w-4/5 h-screen inset-y-0 left-0 bg-black opacity-60 cursor-pointer">
+                class="absolute w-1/6 sm:w-3/6 md:w-3/5 lg:w-2/3 xl:w-9/12 2xl:w-4/5 h-full inset-y-0 left-0 bg-black opacity-60 cursor-pointer">
             </div>
             <div
-                class="absolute w-5/6 sm:w-3/6 md:w-2/5 lg:w-1/3 xl:w-3/12 2xl:w-1/5 h-screen inset-y-0 right-0 bg-white p-3">
+                class="absolute w-5/6 sm:w-3/6 md:w-2/5 lg:w-1/3 xl:w-3/12 2xl:w-1/5 h-full inset-y-0 right-0 bg-white p-3  overflow-y-scroll">
                 <div class="flex flex-col h-full justify-between">
                     <div>
                         <div class="flex justify-between">
@@ -35,23 +35,11 @@ const emit = defineEmits(['closeOverlay'])
                         </div>
                     </div>
                     <div>
-                        <div class="mt-5">
-                            <div class="flex justify-between text-center gap-2 text-gray-700 text-lg">
-                                <div class="bg-gray-100 w-full py-2 cursor-pointer ">
-                                    <Icon name="icon-park-outline:notes" />
-                                </div>
-                                <div class="bg-gray-100 w-full py-2 cursor-pointer">
-                                    <Icon name="icon-park-outline:tag-one" />
-                                </div>
-                                <div class="bg-gray-100 w-full py-2 cursor-pointer">
-                                    <Icon name="icon-park-outline:gift" />
-                                </div>
-                            </div>
-                        </div>
+
                         <div class="my-2">
                             <hr />
                         </div>
-                        <div class="flex flex-col gap-3">
+                        <div class="flex flex-col gap-3 ">
                             <div class="flex justify-between font-bold">
                                 <div>Total</div>
                                 <div>900.00 Taka</div>
@@ -68,6 +56,19 @@ const emit = defineEmits(['closeOverlay'])
                                         class="bg-white hover:bg-black hover:text-white text-sm p-2 w-full border border-black border-1 rounded">VIEW
                                         CART</button>
                                 </NuxtLink>
+                            </div>
+                        </div>
+                        <div class="mt-5">
+                            <div class="flex justify-between text-center gap-2 text-gray-700 text-lg">
+                                <div class="bg-gray-100 w-full py-2 cursor-pointer ">
+                                    <Icon name="icon-park-outline:notes" />
+                                </div>
+                                <div class="bg-gray-100 w-full py-2 cursor-pointer">
+                                    <Icon name="icon-park-outline:tag-one" />
+                                </div>
+                                <div class="bg-gray-100 w-full py-2 cursor-pointer">
+                                    <Icon name="icon-park-outline:gift" />
+                                </div>
                             </div>
                         </div>
                     </div>
