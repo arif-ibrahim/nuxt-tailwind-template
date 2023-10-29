ff3cd9d1a13dd08983eaae3df3909135c7d3b732
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-29 11:45:04 +0600

    working on cart details page

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main-site-padding mt-16">
-        <div class="text-center text-2xl font-medium">Yout Cart</div>
+        <div class="text-center text-2xl font-medium">Your Cart</div>
         <div class="flex flex-col lg:flex-row gap-7 mt-10">
             <div class="lg:w-9/12">
                 <table class="w-full table-auto text-left ">
@@ -35,8 +35,25 @@
                     </tbody>
                 </table>
             </div>
-            <div class="lg:w-3/12 bg-gray-100">
-                add special instruction for your order
+            <div class="lg:w-3/12 bg-gray-100  p-3 flex flex-col gap-5">
+                <div class="flex flex-col gap-2">
+                    <p class="text-xs font-semibold"><label for="instruction">ADD SPECIAL INSTRUCTIONS FOR YOUR
+                            ORDER</label></p>
+                    <textarea class="w-full" id="instruction" name="instruction" rows="2" placeholder=""></textarea>
+                </div>
+                <div>
+                    <div class="mb-2 text-sm font-semibold">COUPON CODE</div>
+                    <input class="w-full p-2" type="text" value="1234">
+                    <div class="mt-1 text-xs text-gray-500">Coupon code will be apllied on the checkout page</div>
+                </div>
+                <div class="flex justify-between font-bold">
+                    <div>TOTAL</div>
+                    <div>12300 TAKA</div>
+                </div>
+                <div>
+                    <button class="bg-black hover:bg-fuchsia-600 text-white text-sm p-2 w-full rounded">PROCEED TO
+                        CHECKOUT</button>
+                </div>
             </div>
         </div>
 
