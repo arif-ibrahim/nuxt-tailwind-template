b60f1acd2d07d1ca246071f8fe2e7c9709fe5510
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-30 13:15:23 +0600

    started working on checkout summary

--- pages/checkout/index.vue
+++ pages/checkout/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main-site-padding">
-        <div class="grid grid-cols-1 md:grid-cols-2 ">
-            <div class=" flex flex-col gap-5 justify-start py-12 pe-10">
+        <div class="grid grid-cols-1 lg:grid-cols-12 ">
+            <div class=" col-span-8 flex flex-col gap-5 lg:justify-start py-12 lg:pe-10">
                 <div class="w-full">
                     <div class="text-xl font-semibold">Contact</div>
                     <input class="mt-2 p-2 w-full placeholder:text-xs border" type="text" placeholder="Mobile phone number">
@@ -24,7 +24,12 @@
                     </div>
                 </div>
             </div>
-            <div class="bg-gray-100 flex justify-end pt-12 ps-10">total</div>
+            <div class="bg-gray-50 pt-3 lg:pt-12 lg:ps-10 lg:col-span-4">
+                <div class="flex justify-between">
+                    <div>name</div>
+                    <div>12300</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
