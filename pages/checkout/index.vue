11b8cb267dc1d2b8b2d99b7a1f470edbb0f3751a
Ibrahim <ibrahim.jatri@gmail.com>
2023-10-30 16:01:14 +0600

    fix design issue

--- pages/checkout/index.vue
+++ pages/checkout/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main-site-padding">
         <div class="grid grid-cols-1 lg:grid-cols-12 ">
-            <div class=" col-span-8 flex flex-col gap-5 lg:justify-start py-12 lg:pe-10">
+            <div class=" col-span-8 flex flex-col gap-5 lg:justify-start py-5 lg:py-12 lg:pe-10">
                 <div class="w-full">
                     <div class="text-xl font-semibold">Contact</div>
                     <input class="mt-2 p-2 w-full placeholder:text-xs border" type="text" placeholder="Mobile phone number">
@@ -24,10 +24,37 @@
                     </div>
                 </div>
             </div>
-            <div class="bg-gray-50 pt-3 lg:pt-12 lg:ps-10 lg:col-span-4">
+            <div class="lg:border-s lg:h-screen pt-3 lg:pt-12 lg:ps-10 lg:col-span-4 flex flex-col gap-5">
                 <div class="flex justify-between">
-                    <div>name</div>
-                    <div>12300</div>
+                    <div class="flex gap-2">
+                        <div>
+                            <img class="h-16 lg:h-24 object-cover object-center" src="/bag3.webp" alt="picture">
+                        </div>
+                        <div>
+                            <div class="text-sm sm:text-sm">Leather bag with chain</div>
+                            <div class="text-xs sm:text-sm text-gray-500">Color: Orange </div>
+                            <div class="text-xs sm:text-sm text-gray-500">Size: M </div>
+                        </div>
+                    </div>
+                    <div class="font-medium text-sm">12300 Taka </div>
+                </div>
+                <div class="flex flex-col gap-1">
+                    <div class="flex justify-between text-sm">
+                        <div>Subtotal</div>
+                        <div class="font-bold">600 Taka</div>
+                    </div>
+                    <div class="flex justify-between text-sm">
+                        <div>Shipping</div>
+                        <div class="font-semibold">60 Taka</div>
+                    </div>
+                    <div class="flex justify-between text-sm">
+                        <div>Coupon</div>
+                        <div class="font-semibold">60 Taka</div>
+                    </div>
+                    <div class="flex justify-between font-bold ">
+                        <div>Total</div>
+                        <div>720 Taka</div>
+                    </div>
                 </div>
             </div>
         </div>
